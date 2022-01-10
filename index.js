a39ee5350d33c96f249a0900038eab2f728abaca
maferamosp <ramosmariaf@hotmail.com>
2022-01-10 11:41:40 -0300

    cambios readme

--- index.js
+++ index.js
@@ -4,51 +4,49 @@ import fetch from 'node-fetch';
 import path from 'path';
 import  Yargs  from 'yargs';
 
-const userPath = process.argv[2];
-const option = Yargs(process.argv.slice(2)).argv;
+const userPath = process.argv[2]; // linea de comando que contiene toda la ruta
+const option = Yargs(process.argv.slice(2)).argv; // para analizar cadenas de opciones
 
-// funcion si es un directorio
-const isDirectory = (dirRoutes)=>{
-    try {
-      const stats = fs.statSync(dirRoutes)
-      return stats.isDirectory();
-    } catch (err) {
-      console.error(err)
-    }
-  };
+// // funcion si es un directorio
+// const isDirectory = (dirRoutes)=>{
+//     try {
+//       const stats = fs.statSync(dirRoutes)
+//       return stats.isDirectory();
+//     } catch (err) {
+//       console.error(err)
+//     }
+//   };
  
 // funcion si es un archivo
 export const isFile = (fileRoute) =>{
     const ext = path.extname(fileRoute); //para encontrar la extension
     if (ext === '.md'){ //verificar si es md
-     const data = fs.readFileSync(fileRoute, { encoding: 'utf8', flag: 'r' })
+     const data = fs.readFileSync(fileRoute, { encoding: 'utf8', flag: 'r' }) //ruta y opiones
        return findLinks(data);
  } else {
      console.log("No es un archivo MarkDown");
  }
 }
 
-//funcion para leer un directorio -------ARREGLAR!!
-export const funReaddir = (files) => {
-  const folder = fs.readdirSync(files);
-  console.log(folder);
-  folder.forEach(file=>{
-
-    const absPath = path.join(files, file);
-    if(isDirectory(absPath)){
-      funReaddir(absPath)
-    }else if (isFile(absPath)){
-      isFile(absPath) // revisar 
-    }
-  })
-}
+// //funcion para leer un directorio -------ARREGLAR!!
+// export const funReaddir = (files) => {
+//   const folder = fs.readdirSync(files);
+//   folder.forEach(file=>{
+//     const absPath = path.join(files, file); //une todos los segmentos y normaliza la ruta
+//     if(isDirectory(absPath)){
+//       funReaddir(absPath)
+//     }else if (isFile(absPath)){
+//       isFile(absPath) // revisar 
+//     }
+//   })
+// }
 
     //----Funcion para encontrar los links-----//
 export const findLinks = (data) =>{
    let getLinks = /(?<!\!)\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g;
-   let getLinksFin = data.matchAll(getLinks)
+   let getLinksFin = data.matchAll(getLinks) // itera por la ER
    const allLinks =  [];
-    for (const match of getLinksFin){
+    for (const match of getLinksFin){ //obtenemos las ocurrencias dentro de ER
       allLinks.push({
         href: match[2], //el link entra en el índice 2
         text: match[1],//el texto del link entra en el índice 1
@@ -59,7 +57,7 @@ export const findLinks = (data) =>{
   }
 
   export const validateLinks = (arrLinks) => {
-    const status = arrLinks.map((element) =>
+    const status = arrLinks.map((element) => //iteramos por el Array
     fetch(element.href) //hago la peticion
     .then((response) =>{ 
       return {
